c23d18ba3140d403e49dcd4faab59389132231b1
Daniela <angied.ruiz@gmail.com>
2021-04-25 22:17:21 -0500

    :sparkles: #91 Vista Home - 1.0 : menu home

---
+++ app/src/main/java/edu/eci/ieti/myapplication/LaunchActivity.java
@@ -0,0 +1,21 @@
+package edu.eci.ieti.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
