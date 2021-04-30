a9b4bf5dcd7e3f7d3b1ad7e3dadecf20ee1028eb
Silenrate <daniwalteros596@gmail.com>
2021-04-29 21:44:01 -0500

    :sparkles:#87 Search Place-1.2: Basic Search View Finalized

--- app/src/main/java/edu/eci/ieti/myapplication/SearchActivity.java
+++ app/src/main/java/edu/eci/ieti/myapplication/SearchActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -24,11 +25,13 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.eci.ieti.myapplication.model.Card;
 import edu.eci.ieti.myapplication.model.LoginWrapper;
 import edu.eci.ieti.myapplication.model.Place;
 import edu.eci.ieti.myapplication.model.Token;
 import edu.eci.ieti.myapplication.services.AuthService;
 import edu.eci.ieti.myapplication.services.PlaceService;
+import edu.eci.ieti.myapplication.ui.CardArrayAdapter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -39,6 +42,8 @@ public class SearchActivity extends AppCompatActivity {
     private EditText editTextPlace;
     private final ExecutorService executorService = Executors.newFixedThreadPool(1);
     private PlaceService placeService;
+    private ListView listView;
+    private CardArrayAdapter cardArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +78,17 @@ public class SearchActivity extends AppCompatActivity {
                     runOnUiThread(() -> {
                         if (response.isSuccessful()) {
                             List<Place> places = response.body();
-                            System.out.println(places);
+                            listView = (ListView) findViewById(R.id.card_listView);
+                            listView.addHeaderView(new View(this));
+                            listView.addFooterView(new View(this));
+                            cardArrayAdapter = new CardArrayAdapter(getApplicationContext(), R.layout.list_item_card);
+                            if (places != null) {
+                                System.out.println(places);
+                                for (Place place : places) {
+                                    cardArrayAdapter.add(new Card(place));
+                                }
+                            }
+                            listView.setAdapter(cardArrayAdapter);
                         } else {
                             editTextPlace.setError("Error de Conexion con BackEnd");
                         }
